ea4d2992ecb98bd616be8647626853bf2ae01d77
Scotton, Luca <luca.scotton@f-secure.com>
2022-01-16 00:46:11 +0200

    refactor: replace std python with numpy

--- connect4rl/board.py
+++ connect4rl/board.py
@@ -1,28 +1,32 @@
-from enum import Enum, auto
+from enum import IntEnum
 from pprint import pprint
-from typing import List, Tuple
+from typing import Tuple
+
+import numpy as np
 
 from connect4rl import exceptions
 
 Position = Tuple[int, int]
 
 
-class State(Enum):
-    empty = auto()
-    player1 = auto()
-    player2 = auto()
+class State(IntEnum):
+    empty = 0
+    player1 = 1
+    player2 = 2
 
 
 class Board:
-    def __init__(self, columns: int = 6, rows: int = 7):
-        self.rows = rows
-        self.columns = columns
-        self._grid = self._init_grid(self.columns)
-        self._win_len = 4
+    def __init__(self, rows: int = 6, columns: int = 7):
+        self._grid = np.full((rows, columns), fill_value=State.empty)
+        self._win_seq_length = 4
 
-    @staticmethod
-    def _init_grid(width) -> List[List[State]]:
-        return [[] for _ in range(width)]
+    @property
+    def rows(self) -> int:
+        return self._grid.shape[0]
+
+    @property
+    def columns(self) -> int:
+        return self._grid.shape[1]
 
     def player1_move(self, column):
         player = State.player1
@@ -37,20 +41,21 @@ class Board:
     def _play_move(self, player: State, column) -> Position:
         if not (0 <= column < self.columns):
             raise exceptions.BoardError(f"Column {column} is not valid")
-        column_list = self._grid[column]
-        if len(column_list) > self.rows:
+        empty_slots: np.ndarray = np.nonzero(self._grid[:, column] == 0)[0]
+        if not empty_slots.size:
             raise exceptions.BoardError(f"Column {column} is already full")
-        column_list.append(player)
-        return column, len(column_list) - 1
+        first_empty_idx = empty_slots[0]
+        self._grid[first_empty_idx, column] = player
+        return first_empty_idx, column
 
     def check_victory(self, position: Position, player: State) -> bool:
         horizontal = (
             1
-            + self._count_coin(position, player, (1, 0))
-            + self._count_coin(position, player, (-1, 0))
+            + self._count_coin(position, player, (0, 1))
+            + self._count_coin(position, player, (0, -1))
         )
 
-        vertical = 1 + self._count_coin(position, player, (0, -1))
+        vertical = 1 + self._count_coin(position, player, (-1, 0))
         diag_right = (
             1
             + self._count_coin(position, player, (1, 1))
@@ -58,15 +63,15 @@ class Board:
         )
         diag_left = (
             1
-            + self._count_coin(position, player, (-1, 1))
             + self._count_coin(position, player, (1, -1))
+            + self._count_coin(position, player, (-1, 1))
         )
 
         if (
-            horizontal >= self._win_len
-            or vertical >= self._win_len
-            or diag_right >= self._win_len
-            or diag_left >= self._win_len
+            horizontal >= self._win_seq_length
+            or vertical >= self._win_seq_length
+            or diag_right >= self._win_seq_length
+            or diag_left >= self._win_seq_length
         ):
             return True
         return False
@@ -74,22 +79,22 @@ class Board:
     def _count_coin(
         self, position: Position, player: State, traslation: Position
     ) -> int:
-        col, row = position
-        trasl_col, trasl_row = traslation
-        col += trasl_col
+        row, col = position
+        trasl_row, trasl_col = traslation
         row += trasl_row
+        col += trasl_col
 
         if col < 0 or row < 0:
             return 0
 
         try:
-            player_coin = self._grid[col][row]
+            player_coin = self._grid[row, col]
         except IndexError:
             return 0
 
         if player_coin == player:
-            return 1 + self._count_coin((col, row), player, traslation)
+            return 1 + self._count_coin((row, col), player, traslation)
         return 0
 
     def display(self):
-        pprint(self._grid)
+        pprint(self._grid[::-1, :])
